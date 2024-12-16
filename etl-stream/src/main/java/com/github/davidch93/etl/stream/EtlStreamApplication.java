d575e788be30e463772443f57d8ee3ece52e1c55
David Christianto <david.christianto@bukalapak.com>
2024-12-16 14:02:48 +0700

    [20241216] Update Javadoc for the main class ## Updates - Updated Javadoc for `EtlStreamApplication`.

--- etl-stream/src/main/java/com/github/davidch93/etl/stream/EtlStreamApplication.java
+++ etl-stream/src/main/java/com/github/davidch93/etl/stream/EtlStreamApplication.java
@@ -5,16 +5,29 @@ import com.github.davidch93.etl.stream.options.StreamOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 
 /**
- * The main entrypoint of the stream pipeline.
+ * The {@code EtlStreamApplication} serves as the main entry point for the ETL Stream Pipeline.
+ * <p>
+ * This class initializes and starts the pipeline by:
+ * <ul>
+ *   <li>Parses command-line arguments into {@link StreamOptions} using {@link PipelineOptionsFactory}.</li>
+ *   <li>Validates the parsed options for correctness.</li>
+ *   <li>Creates and executes the pipeline by delegating to {@link StreamCLI}</li>
+ * </ul>
+ *
+ * <p>
+ * The application leverages Apache Beam for stream processing and expects configuration
+ * to be passed as command-line arguments.
+ * </p>
  *
  * @author david.christianto
  */
 public class EtlStreamApplication {
 
     /**
-     * Start the stream pipeline.
+     * Starts the ETL stream pipeline.
      *
-     * @param args the input arguments
+     * @param args the input arguments for configuring the pipeline. These should match
+     *             the options defined in {@link StreamOptions}.
      */
     public static void main(String... args) {
         StreamOptions options = PipelineOptionsFactory.fromArgs(args)
